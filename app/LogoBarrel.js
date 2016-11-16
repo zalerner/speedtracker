3fe1f8995f3bf71eb4d084283ea6c68fe7e4d9f1
zalerner <zack.lerner@barrelny.com>
2016-11-16 11:11:35 -0500

    Create LogoBarrel.js

---
+++ app/LogoBarrel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+class LogoBarrel extends React.Component {
+  render() {
+    return (
+      <img src="https://www.barrelny.com/wp-content/uploads/barrel-white.png" />
+    )
+  }
+}
+
+export default LogoBarrel
